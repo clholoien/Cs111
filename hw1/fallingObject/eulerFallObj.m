ef8370d11b28f5b24f141b4a4168535b3baab0a4
Kelly Bielaski <knbielaski@yahoo.com>
2016-10-25 21:25:31 -0700

    euler falling object works

---
+++ hw1/fallingObject/eulerFallObj.m
@@ -0,0 +1,35 @@
+function outvar = eulerFallObj()
+    t=0;
+    tfinal=15;
+    u=0;
+    exact=0;
+    dt=.3;
+    g=9.81;
+    cd=.25;
+    m=75;
+    uplot=u;
+    tplot=t;
+    exactplot=exact;
+    error=0;
+    while(t<tfinal)
+        if(t+dt>tfinal)
+            dt=tfinal-t;
+        end
+        u=u+dt*(g-cd/m*u*u);
+        exact=sqrt(g*m/cd)*tanh(sqrt(g*cd/m)*t);
+        if(abs(u-exact)>error)
+            error=abs(u-exact);
+        end
+        t=t+dt;
+        exactplot = [exactplot exact]; 
+        uplot = [uplot u];
+        tplot = [tplot t];
+    end
+    %plot(tplot,exactplot,'blue');
+    %plot(tplot,uplot,'.');
+    plotyy(tplot,uplot,tplot,exactplot);
+    xlabel('time');
+    ylabel('position');
+
+    
+  
---
+++ hw1/fallingObject/rk2FallObj.m
@@ -0,0 +1,32 @@
+function outvar = rk2FallObj()
+    t=0;
+    tfinal=15;
+    u=0;
+    exact=0;
+    dt=.3;
+    g=9.81;
+    cd=.25;
+    m=75;
+    uplot=u;
+    tplot=t;
+    exactplot=exact;
+    error=0;
+    while(t<tfinal)
+        if(t+dt>tfinal)
+            dt=tfinal-t;
+        end
+        u=u+dt(%this is where I put a bunch of equations);
+        exact=sqrt(g*m/cd)*tanh(sqrt(g*cd/m)*t);
+        if(abs(u-exact)>error)
+            error=abs(u-exact);
+        end
+        t=t+dt;
+        exactplot = [exactplot exact]; 
+        uplot = [uplot u];
+        tplot = [tplot t];
+    end
+    %plot(tplot,exactplot,'blue');
+    %plot(tplot,uplot,'.');
+    plotyy(tplot,uplot,tplot,exactplot);
+    xlabel('time');
+    ylabel('position');
