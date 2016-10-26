8d8e15514183cd943256cae72358c52d7edbcd96
Kelly Bielaski <knbielaski@yahoo.com>
2016-10-25 23:02:18 -0700

    rk4 works

--- hw1/fallingObject/rk4FallObj.m
+++ hw1/fallingObject/rk4FallObj.m
@@ -11,14 +11,11 @@ function outvar = rk2FallObj()
     tplot=t;
     exactplot=exact;
     error=0;
-    u' = (g-cd/m-u*u);
-    
     while(t<tfinal)
         if(t+dt>tfinal)
             dt=tfinal-t;
         end
-        
-        u=u+dt*(g-cd/m-u*u)+dt*dt/2*(-cd/m)*(2*u*(g-cd/m*u*u))+dt*dt*dt/6*(-2*cd/m*(g-cd/m*u*u)+u*(-cd/m*2*u))+dt*dt*dt*dt/24*(-2*cd/m*(-cd/m*u*2)+(-cd/m*4)*u);
+        u=u+dt*(g-cd/m*u*u)+dt*dt/2*(-cd/m)*(2*u*(g-cd/m*u*u))+dt*dt*dt/6*(-2*cd/m*((g-cd/m*u*u)+u*(-cd/m*2*u)))+dt*dt*dt*dt/24*(-2*cd/m*(-cd/m*u*2)+(-cd/m*4)*u);
         exact=sqrt(g*m/cd)*tanh(sqrt(g*cd/m)*t);
         if(abs(u-exact)>error)
             error=abs(u-exact);
